939da2fcbc9e220b155801a1deedfc4252d30c27
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2021-06-17 16:35:46 +1000

    Add Word2Vec embeddings

--- utils/java_ast/java_ast_encoder.py
+++ utils/java_ast/java_ast_encoder.py
@@ -3,9 +3,22 @@ from __future__ import annotations
 import argparse
 import os.path
 import time
+import multiprocessing
 from typing import List, Tuple
 from java_ast_provider import JavaST, ParsedNode
 from tqdm import tqdm
+from gensim.models import Word2Vec
+from gensim import utils
+
+
+class Corpus:
+    def __init__(self, path):
+        self.path = path
+
+    def __iter__(self):
+        with open(self.path, 'r', encoding='utf-8') as fp:
+            for line in fp:
+                yield utils.simple_preprocess(line)
 
 
 class JavaSTEncoder:
@@ -69,32 +82,97 @@ class JavaSTEncoder:
         return list(corpus), list(vocabulary)
 
     @staticmethod
-    def sass(output_dir_path: str, paths: List[str] = None, source_codes: List[str] = None) -> None:
+    def sass(output_dir: str, paths: List[str] = None, source_codes: List[str] = None) -> str:
         try:
             corpus, vocab = JavaSTEncoder.sequentialize(paths=paths, source_codes=source_codes)
             if len(corpus) > 0 and len(vocab) > 0:
-                corpus_path = os.path.join(output_dir_path, 'java.corpus')
-                vocab_path = os.path.join(output_dir_path, 'java.vocab')
+                corpus_path = os.path.join(output_dir, 'java.corpus')
 
                 with open(corpus_path, 'w', encoding='utf-8') as fp:
                     print('\n'.join(corpus), file=fp)
                 print(f'Generated {corpus_path}')
 
-                with open(vocab_path, 'w', encoding='utf-8') as fp:
-                    print('\n'.join(vocab), file=fp)
-                print(f'Generated {vocab_path}')
+                return corpus_path
         except:
-            print(f'Couldn\'t generate corpus and vocabulary')
+            print(f'Couldn\'t generate corpus')
+
+    @staticmethod
+    def w2v(corpus_path: str) -> Word2Vec:
+        cores = multiprocessing.cpu_count()
+        w2v_model = Word2Vec(sentences=Corpus(corpus_path),
+                             workers=cores-1, min_count=1,
+                             epochs=30, vector_size=192, sample=1e-3)
+        return w2v_model
 
 
-def generate_sequence(args) -> None:
-    path = args.path
-    JavaSTEncoder.sass(path, paths=[path])
+def generate_corpus(path: str, output_dir: str = None) -> None:
+    output_dir = output_dir or (path if os.path.isdir(path) else os.path.dirname(path))
+    JavaSTEncoder.sass(output_dir, paths=[path])
+
+
+def learn_w2v_embeddings(path: str, output_dir: str = None):
+    output_dir = output_dir or (path if os.path.isdir(path) else os.path.dirname(path))
+    w2v_model = JavaSTEncoder.w2v(path)
+    name = 'java.w2v'
+    if not os.path.isdir(path):
+        pre, ext = os.path.splitext(path)
+        name = f'{pre}{ext}.w2v'
+    model_path = os.path.join(output_dir, name)
+    try:
+        w2v_model.save(fname_or_handle=model_path)
+        print(f'Generated {model_path}')
+    except:
+        print(f'Couldn\'t save Word2Vec model')
+
+
+def load_w2v_model(path: str):
+    try:
+        w2v_model = Word2Vec.load(fname=path)
+        pre, ext = os.path.splitext(os.path.basename(path))
+        sim_path = os.path.join(os.path.dirname(path), pre + ext + '.sim')
+        print(w2v_model.wv.index_to_key)
+        print(f'Total words: {len(w2v_model.wv.index_to_key)}')
+        with open(sim_path, 'w', encoding='utf-8') as fp:
+            for i, w in enumerate(w2v_model.wv.index_to_key):
+                sim = "\n".join(map(str, w2v_model.wv.most_similar(positive=[w])))
+                print(f'{w}[{sim}]\n', file=fp)
+        print(f'Generated {sim_path}')
+    except:
+        print(f'Couldn\'t load {path}')
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Sequentialize Java AST.')
-    argparser.add_argument('path',
-                           help='path to .java file',
+    argparser.add_argument('-jp', '--java', metavar='PATH',
+                           help='path to `.java` file or directory containing `.java` files, generates corpus to [-od]',
                            action='store')
-    generate_sequence(argparser.parse_args())
+    argparser.add_argument('-cp', '--corpus', metavar='PATH',
+                           help='path to `.corpus` file to learn embeddings from',
+                           action='store')
+    argparser.add_argument('-od', '--output', metavar='PATH',
+                           help='path to output directory for [-jp, --java], [-w2v], [-w2v_sim]',
+                           action='store')
+    argparser.add_argument('-w2v',
+                           help='Word2Vec',
+                           action='store_const',
+                           const=True,
+                           default=False)
+    argparser.add_argument('-ft',
+                           help='fastText',
+                           action='store_const',
+                           const=True,
+                           default=False)
+    argparser.add_argument('-w2v_sim', metavar='PATH',
+                           help='path to Word2Vec model to generate similarities from',
+                           action='store')
+
+    args = argparser.parse_args()
+    if args.java:
+        generate_corpus(args.java, output_dir=args.output)
+
+    if args.corpus:
+        if args.w2v:
+            learn_w2v_embeddings(args.corpus, output_dir=args.output)\
+
+    if args.w2v_sim:
+        load_w2v_model(args.w2v_sim)
